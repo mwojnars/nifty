01d1a4d383fac16960274e38b68044e55c7f2d09
Marcin Wojnarski <mwojnars@ns.onet.pl>
2019-11-20 21:10:15 +0100

    sizeof() added

--- util.py
+++ util.py
@@ -513,6 +513,16 @@ def heapmerge(*inputs):
     return heapq.merge(*iterables)
 
 
+def sizeof(obj):
+    """Estimates total memory usage of (possibly nested) `obj` by recursively calling sys.getsizeof() for list/tuple/dict/set containers
+       and adding up the results. Does NOT handle circular object references!
+    """
+    size = sys.getsizeof(obj)
+    if isinstance(obj, dict): return size + sum(map(sizeof, obj.keys())) + sum(map(sizeof, obj.values()))
+    if isinstance(obj, (list, tuple, set, frozenset)): return size + sum(map(sizeof, obj))
+    return size
+
+
 #####################################################################################################################################################
 ###
 ###   OBJECTS
